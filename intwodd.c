5a548c29160943c7f00868a28ced4dc54c0417b4
14-11-1997 <kirthikas.cse@gmail.com>
2017-06-30 14:50:42 +0530

    Add files via upload

---
+++ intwodd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int l,t,k;
+printf("Enter the start interval");
+scanf("%d",&l);
+printf("enter the ending interval");
+scanf("%d",&t);
+for(k=0;k<=t;k++)
+if(k%2!=0)
+{
+printf(" %d",k);
+}
+return 0;
+}
